68905fbf99f0e87db98baa439f65038b83ebaa59
zar4za <zar4z4@yandex.ru>
2022-10-22 15:15:23 +0300

    Миграция с zar4za/marketplace

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using CsMarket.Services;
+
+namespace CsMarket.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserService _userService;
+
+        public AuthController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet("openid-request")]
+        public IActionResult RequestOpenId(string verifyLink)
+        {
+            var link = _userService.GetRedirectLink(verifyLink);
+            var payload = new
+            {
+                link
+            };
+            return Ok(payload);
+        }
+
+        [HttpPost("openid-verify")]
+        public async Task<IActionResult> VerifyOpenId([FromBody] string query)
+        {
+            var steamid = await _userService.VerifyOpenId(query);
+
+            if (steamid == null) return Unauthorized();
+
+            var jwt = _userService.GetUserToken(steamid);
+
+            return Ok(new { jwt });
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,19 +1,22 @@
+using CsMarket.Helpers;
+using CsMarket.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddHttpClient();
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddJwtAuthentication(builder.Configuration);
+builder.Services.AddTransient<UserService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseRouting();
 app.UseAuthorization();
-app.MapControllers();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 app.Run();
